c79b265e4647f85d05e85989d60107657aded468
BrazilGuyLol <96142970+BrazilGuyLol@users.noreply.github.com>
2022-03-17 13:34:16 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -41,7 +41,7 @@ class CreditsState extends MusicBeatState
 	{
 		#if desktop
 		// Updating Discord Rich Presence
-		DiscordClient.changePresence("In the Menus", null);
+		DiscordClient.changePresence("Corrupting The Menus", null);
 		#end
 
 		persistentUpdate = true;
@@ -84,7 +84,7 @@ class CreditsState extends MusicBeatState
 		        ['Android Port'],
 		        ['Saw (M.A. Jigsaw)','saw','Main Coder','https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ', 'F73838'],
                         ['A New Universe Team'],
-                        ['Brazil Guy', 'guy','Coder,Cover Maker,Spriter,Owner','https://youtube.com/channel/UCQ4XeytQTHM_cfE85ho4Yjw','',
+                        ['Brazil Guy', 'guy','Coder,Cover Maker,Spriter,Owner','https://youtube.com/channel/UCQ4XeytQTHM_cfE85ho4Yjw','#2EFE2E',
                         ['Starzs', 'star','Coder, Spriter','https://youtube.com/c/Starz1991',''],
 		        [''],
 			['Psych Engine Team'],
